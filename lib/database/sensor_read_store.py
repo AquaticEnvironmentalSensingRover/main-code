10af9d933d14189035e1af3c8bf0eef4732be9ce
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-18 18:10:58 -0400

    Add in-dev sensor threading library

---
+++ lib/database/sensor_read_store.py
@@ -0,0 +1,12 @@
+import threading
+
+class SensorReadStore(threading.Thread):
+    def __init__(self, sensorReadFunction, dataStoreFunction):
+        pass
+        
+    def run():
+        # 1. Run and save value from "sensorReadFunction"
+        # 2. Store by doing some formatting and passing as the only argument
+        #    to "dataStoreFunction"
+        
+        pass
