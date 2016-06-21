6427bda4fa520469d1538a64b5b0ea479ad4834d
luciengaitskell <luciengaitskell@users.noreply.github.com>
2016-06-21 18:22:20 -0400

    Add handling of consecutive errors to pressure sensor write test

--- pressure_sensor_write_test.py
+++ pressure_sensor_write_test.py
@@ -16,6 +16,9 @@ sensor = MS5803()
 #  message : <String>
 #}
 
+MAX_ERRORS = 10
+errorAmount = 0
+
 while True:
     try:
         data = sensor.read()
@@ -23,6 +26,14 @@ while True:
                     , "param" : {"pressure":data["mbar"],"temp":data["temp"]}
                     , "paramunit": {"pressure":"mbar", "temp":"degC"}
                     , "comments" : "testing", "tags": ["pressure", "test"]})
+                    
+        errorAmount = 0
         time.sleep(1)
     except IOError:
         print("Read write error")
+        
+        errorAmount += 1
+        if errorAmount>=MAX_ERRORS:
+            print("The amount of consecutive errors exceded the max amount "
+                + " (" + MAX_ERRORS + ")")
+            break
