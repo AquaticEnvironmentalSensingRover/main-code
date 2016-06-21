402617c4473612a66bec402617901aec0a64746e
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-21 15:54:23 -0400

    Add pressure sensor to sensor store

--- sensor_store.py
+++ sensor_store.py
@@ -1,20 +1,23 @@
 from lib.sensors.mcp9808 import MCP9808
+from lib.sensors.ms5803 import MS5803
 from lib.sensors.gps_read import GPSRead
 from lib.database.mongo_write import MongoWrite
 import sys
 import datetime, time
 
-tempSensors = []
+mongo = MongoWrite(datetime.now().strftime("AESR-%Y%m%d:%H%M%S"), "data")
 
+tempSensors = []
 tempSensors.append(MCP9808(0x18))
 tempSensors.append(MCP9808(0x18+4))
 tempSensors.append(MCP9808(0x18+1))
 tempSensors.append(MCP9808(0x18+2))
 tempSensors.append(MCP9808(0x18+3))
 
+pressureSensor = MS5803()
+
 gpsSensor = GPSRead()
 
-mongo = MongoWrite(datetime.now().strftime("AESR-%Y%m%d:%H%M%S"), "data")
 
 #{
 #  ver : <float>
@@ -28,6 +31,7 @@ mongo = MongoWrite(datetime.now().strftime("AESR-%Y%m%d:%H%M%S"), "data")
 #}
 
 while True:
+    # GPS
     try:
         location = gpsSensor.readLocationData()
         mongo.write({"atype":"GPS", "vertype": 1.0, "ts": time.time()
@@ -37,6 +41,16 @@ while True:
     except ValueError:
         pass
     
+    
+    # PRESSURE Sensor
+    data = pressureSensor.read()
+    mongo.write({"atype":"PRESR", "vertype": 1.0, "ts": time.time()
+                , "param" : {"pressure":data["mbar"],"temp":data["mbar"]}
+                , "paramunit": {"pressure":"mbar", "temp":"degC"}
+                , "comments" : "testing", "tags": ["pressure", "test"]})
+        
+    
+    # TEMPERATURE Sensor
     for ii, tempSensor in enumerate(tempSensors):
         mongo.write({"atype":"TEMP", "itype": ii, "vertype": 1.0
                     , "ts": time.time(), "param" : tempSensor.read()
