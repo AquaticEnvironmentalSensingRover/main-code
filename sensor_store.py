ed49db6d64f8671569a61483d8567fd8e96d6337
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-25 14:27:39 -0400

    Catch all exceptions (except KeyboardInterrupts) in sensor store

--- sensor_store.py
+++ sensor_store.py
@@ -3,11 +3,11 @@ from lib.sensors.ms5803 import MS5803
 from lib.sensors.gps_read import GPSRead
 from lib.sensors.mb7047 import MB7047
 from lib.database.mongo_write import MongoWrite
-import sys
 from datetime import datetime
-import time
+import time, sys
 
-mongo = MongoWrite(datetime.now().strftime("AESR-%Y%m%d:%H%M%S"), "data")
+mongo = MongoWrite(datetime.now().strftime("AESR_%Y%m%dT%H%M%S"), "data")
+print "Connected to mongoDB server successfully!"
 
 tempSensors = []
 tempSensors.append(MCP9808(0x18))
@@ -18,10 +18,11 @@ tempSensors.append(MCP9808(0x18+3))
 
 pressureSensor = MS5803()
 
-gpsSensor = GPSRead()
-
 sonarSensor = MB7047()
 
+gpsSensor = GPSRead()
+
+print "Sensors created successfully!"
 #{
 #  ver : <float>
 #  atype: <String>,
@@ -33,53 +34,62 @@ sonarSensor = MB7047()
 #  message : <String>
 #}
 
-while True:
-    # GPS
-    try:
-        location = gpsSensor.readLocationData()
-        mongo.write({"atype":"GPS", "vertype": 1.0, "ts": time.time()
-                    , "param" : {"lat":location.lat,"lon":location.lon}
-                    , "paramunit": "{degLat,degLon}", "comments" : "testing"
-                    , "tags": ["gps", "test"]})
-    except ValueError:
-        pass
-    except IOError:
-        mongo.write({"atype":"ALERT", "vertype": 1.0, "ts": time.time()
-                    , "param":"GPS failed to read"})
-    
-    # PRESSURE Sensor
-    try:
-        data = pressureSensor.read()
-        mongo.write({"atype":"PRESR", "vertype": 1.0, "ts": time.time()
-                    , "param" : {"pressure":data["mbar"],"temp":data["temp"]}
-                    , "paramunit": {"pressure":"mbar", "temp":"degC"}
-                    , "comments" : "testing", "tags": ["pressure", "test"]})
-    except IOError:
-        mongo.write({"atype":"ALERT", "vertype": 1.0, "ts": time.time()
-                    , "param":"Pressure sensor failed to read"})
+try:
+    while True:
+        # GPS
+        try:
+            location = gpsSensor.readLocationData()
+            mongo.write({"atype":"GPS", "vertype": 1.0, "ts": time.time()
+                        , "param" : {"lat":location.lat,"lon":location.lon}
+                        , "paramunit": "{degLat,degLon}", "comments" : "testing"
+                        , "tags": ["gps", "test"]})
+        except KeyboardInterrupt:
+            raise sys.exc_info()
+        except:
+            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"GPS"
+                        , "ts": time.time(), "param":sys.exc_info()})
         
-    
-    # TEMPERATURE Sensor
-    try:
-        for ii, tempSensor in enumerate(tempSensors):
-            mongo.write({"atype":"TEMP", "itype": ii, "vertype": 1.0
-                        , "ts": time.time(), "param" : tempSensor.read()
-                        , "paramunit": "degC", "comments" : "testing"
-                        , "tags": ["temp", "test"]})
-    except IOError:
-        mongo.write({"atype":"ALERT", "vertype": 1.0, "ts": time.time()
-                    , "param":"Temperature sensor failed to read"})
-
-                    
-    # SONAR Sensor
-    try:
-        data = sonarSensor.read()
+        # PRESSURE Sensor
+        try:
+            data = pressureSensor.read()
+            mongo.write({"atype":"PRESR", "vertype": 1.0, "ts": time.time()
+                        , "param" : {"pressure":data["mbar"],"temp":data["temp"]}
+                        , "paramunit": {"pressure":"mbar", "temp":"degC"}
+                        , "comments" : "testing", "tags": ["pressure", "test"]})
+        except KeyboardInterrupt:
+            raise sys.exc_info()
+        except:
+            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"PRESR"
+                        , "ts": time.time(), "param":sys.exc_info()})
         
-        mongo.write({"atype":"SONAR", "vertype": 1.0, "ts": time.time()
-                    , "param" : data, "paramunit": "cm"
-                    , "comments" : "testing", "tags": ["sonar", "depth", "test"]})
-    except IOError:
-        mongo.write({"atype":"ALERT", "vertype": 1.0, "ts": time.time()
-                    , "param":"Sonar sensor failed to read"})
+        # TEMPERATURE Sensor
+        try:
+            for ii, tempSensor in enumerate(tempSensors):
+                mongo.write({"atype":"TEMP", "itype": ii, "vertype": 1.0
+                            , "ts": time.time(), "param" : tempSensor.read()
+                            , "paramunit": "degC", "comments" : "testing"
+                            , "tags": ["temp", "test"]})
+        except KeyboardInterrupt:
+            raise sys.exc_info()
+        except:
+            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"TEMP"
+                        , "ts": time.time(), "param":sys.exc_info()})
     
-    time.sleep(1)
+                        
+        # SONAR Sensor
+        try:
+            data = sonarSensor.read()
+            
+            mongo.write({"atype":"SONAR", "vertype": 1.0, "ts": time.time()
+                        , "param" : data, "paramunit": "cm"
+                        , "comments" : "testing", "tags": ["sonar", "depth", "test"]})
+        except KeyboardInterrupt:
+            raise sys.exc_info()
+        except:
+            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"SONAR"
+                        , "ts": time.time(), "param":sys.exc_info()})
+                
+        time.sleep(1)
+except KeyboardInterrupt:
+    gpsSensor.close()
+    raise sys.exc_info()
