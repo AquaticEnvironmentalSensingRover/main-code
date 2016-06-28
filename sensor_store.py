728deb335f21a688ccea88719580c587d063b225
luciengaitskell <luciengaitskell@users.noreply.github.com>
2016-06-28 17:05:56 -0400

    Add initiated device list and function for reading and storing device data in sensor store

--- sensor_store.py
+++ sensor_store.py
@@ -7,34 +7,91 @@ from lib.database.mongo_write import MongoWrite
 from datetime import datetime
 import time, sys
 
+def valueReplaceScan(value):
+    if type(value) == type(list()):
+        newValue = []
+        for ii in range(len(value)):
+            newValue.append(valueReplaceScan(value[ii]))
+    elif not value == None:
+            newValue = "Ok"
+    else:
+        newValue = None
+    
+    return newValue
+
 def createDevice(deviceType, sensorConstructor, *args, **kwargs):
     try:
         device = sensorConstructor(*args, **kwargs)
         print(deviceType + " device was successfully initialized!")
-        return device
     except:
         print "Failed setting up " + deviceType + " device"
-        return None
+        device = None
+    finally:
+        return device
+
 
 mongo = MongoWrite(datetime.now().strftime("AESR_%Y%m%dT%H%M%S"), "data")
 print "Connected to mongoDB server successfully!"
 
+
+def readDevice(device, readFunctionName, atype, paramUnit
+                , comments = [], tags = [], itype = None):
+    try:
+        if not device == None:
+            param = getattr(device, readFunctionName)()
+            
+            writeData = {"atype": atype, "vertype": 1.0, "ts": time.time()
+                        , "param": param, "paramunit": paramUnit
+                        , "comments": comments, "tags": tags}
+            
+            if itype == None:
+                writeData["itype"] = itype
+            
+            mongo.write(writeData)
+    except KeyboardInterrupt:
+        raise sys.exc_info()
+    except:
+        mongo.write({"atype": "ALERT", "vertype": 1.0, "itype": atype
+                    , "ts": time.time(), "param": str(sys.exc_info()[0])})
+
+# =================Sensor Creation=================
+devices = {}
+
 tempSensors = []
-tempSensors.append(createDevice("temperature 1", MCP9808, 0x18))
-tempSensors.append(createDevice("temperature 2", MCP9808, 0x18+4))
-tempSensors.append(createDevice("temperature 3", MCP9808, 0x18+1))
-tempSensors.append(createDevice("temperature 4", MCP9808, 0x18+2))
-tempSensors.append(createDevice("temperature 5", MCP9808, 0x18+3))
+tempSensors.append(createDevice("Temperature 1", MCP9808, 0x18))
+tempSensors.append(createDevice("Temperature 2", MCP9808, 0x18+4))
+tempSensors.append(createDevice("Temperature 3", MCP9808, 0x18+1))
+tempSensors.append(createDevice("Temperature 4", MCP9808, 0x18+2))
+tempSensors.append(createDevice("Temperature 5", MCP9808, 0x18+3))
+devices["temperature"] = tempSensors
 
-pressureSensor = createDevice("pressure", MS5803)
+devices["pressure"] = createDevice("Pressure", MS5803)
 
-sonarSensor = createDevice("sonar", MB7047)
+devices["sonar"] = createDevice("Sonar", MB7047)
 
-adcDevice = createDevice("ADC", ADS1115)
+devices["adc"] = createDevice("ADC", ADS1115)
 
-gpsSensor = createDevice("GPS", GPSRead)
+devices["gps"] = createDevice("GPS", GPSRead)
 
-print "Sensors created!"
+# Print sensors
+print "============================" 
+print "\nSensors:"
+
+# Get keys and values of the "devices" dictionary separately
+keys = []
+values = []
+for k,v in devices.items():
+    keys.append(k)
+    values.append(v)
+   
+# Run value replace scan with the values to format for printing
+printValues = valueReplaceScan(values)
+
+# Use printing values for "devicesPrint"
+for ii, key in enumerate(keys):
+    print str(key) + ": " + str(printValues[ii])
+print "============================\n"
+
 #{
 #  ver : <float>
 #  atype: <String>,
@@ -49,77 +106,33 @@ print "Sensors created!"
 try:
     while True:
         # GPS
-        try:
-            if not gpsSensor == None:
-                location = gpsSensor.readLocationData()
-                mongo.write({"atype":"GPS", "vertype": 1.0, "ts": time.time()
-                            , "param" : {"lat":location.lat,"lon":location.lon}
-                            , "paramunit": "{degLat,degLon}", "comments" : "testing"
-                            , "tags": ["gps", "test"]})
-        except KeyboardInterrupt:
-            raise sys.exc_info()
-        except:
-            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"GPS"
-                        , "ts": time.time(), "param":str(sys.exc_info()[0])})
-        
+        readDevice(devices["gps"], "readLocationData", atype = "GPS"
+                    , paramUnit = {"lat": "latitude", "lon": "longitude"}
+                    , comments = ["Brick Yard Pond"])
+                            
         # PRESSURE Sensor
-        try:
-            if not pressureSensor == None:
-                pData = pressureSensor.read()
-                mongo.write({"atype":"PRESR", "vertype": 1.0, "ts": time.time()
-                            , "param" : {"pressure":pData["mbar"],"temp":pData["temp"]}
-                            , "paramunit": {"pressure":"mbar", "temp":"degC"}
-                            , "comments" : "testing", "tags": ["pressure", "test"]})
-        except KeyboardInterrupt:
-            raise sys.exc_info()
-        except:
-            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"PRESR"
-                        , "ts": time.time(), "param":str(sys.exc_info()[0])})
-        
+        readDevice(devices["pressure"], "read", atype = "PRESR"
+                    , paramUnit = {"pressure":"mbar", "temp":"degC"}
+                    , comments = ["Brick Yard Pond"])
+                
         # TEMPERATURE Sensor
-        
-        for ii, tempSensor in enumerate(tempSensors):
-            try:
-                if not tempSensor == None:
-                    tData = tempSensor.read()
-                    mongo.write({"atype":"TEMP", "itype": ii, "vertype": 1.0
-                                , "ts": time.time(), "param" : tData
-                                , "paramunit": "degC", "comments" : "testing"
-                                , "tags": ["temp", "test"]})
-            except KeyboardInterrupt:
-                raise sys.exc_info()
-            except:
-                mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"TEMP"
-                            , "ts": time.time()
-                            , "param":[ii, str(sys.exc_info()[0])]})
+        for ii, tempSensor in enumerate(devices["temperature"]):
+            readDevice(tempSensor, "read", atype = "TEMP", itype = ii
+                    , paramUnit = "degC"
+                    , comments = ["Brick Yard Pond"])
         
         # SONAR Sensor
-        try:
-            if not sonarSensor == None:
-                sData = sonarSensor.read()
-                mongo.write({"atype":"SONAR", "vertype": 1.0, "ts": time.time()
-                            , "param" : sData, "paramunit": "cm"
-                            , "comments" : "testing", "tags": ["sonar", "depth", "test"]})
-        except KeyboardInterrupt:
-            raise sys.exc_info()
-        except:
-            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"SONAR"
-                        , "ts": time.time(), "param":str(sys.exc_info()[0])})
+        readDevice(devices["sonar"], "read", atype = "SONAR"
+                    , paramUnit = "cm"
+                    , comments = ["Brick Yard Pond"])
+        
         
         # ADS for Optical Dissolved Oxygen Sensor
-        try:
-            if not adcDevice == None:
-                oData = adcDevice.read()
-                mongo.write({"atype":"ODO", "vertype": 1.0, "ts": time.time()
-                            , "param" : oData, "paramunit": "rawADC"
-                            , "comments" : "testing"
-                            , "tags": ["oxygen", "dissolved", "ADC"]})
-        except KeyboardInterrupt:
-            raise sys.exc_info()
-        except:
-            mongo.write({"atype":"ALERT", "vertype": 1.0, "itype":"ODO"
-                        , "ts": time.time(), "param":str(sys.exc_info()[0])})
-
+        readDevice(devices["adc"], "read", atype = "ODO"
+                    , paramUnit = "rawADC"
+                    , comments = ["Brick Yard Pond"])
+        
         time.sleep(1)
 finally:
-    gpsSensor.close()
+    if not devices["gps"] == None:
+        devices["gps"].close()
