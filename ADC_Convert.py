4bf1606b67560d30012f8aeeb59b1d295cc1e08c
Ben <ben.nault@wheelergmail.org>
2016-07-06 13:48:52 -0400

    Added a program to output live mg/L values from ODO on the pi

---
+++ ADC_Convert.py
@@ -0,0 +1,9 @@
+from lib.sensors.ads1115 import ADS1115
+
+x = ADS1115()
+
+while True:
+    a = x.read() #reads ADC
+    c = a * (5/26665.8528646) #Converts to a 0-5V range
+    output = (c * 4.444) - .4444 #Converts to mg/L based of Vernier's scale
+    print output, a
