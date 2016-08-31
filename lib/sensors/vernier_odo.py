87a287860c29b43bd0d02eb21e325fc0036e762c
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-08-31 14:14:12 -0400

    Fix python styling of 'vernier_odo'

--- lib/sensors/vernier_odo.py
+++ lib/sensors/vernier_odo.py
@@ -1,21 +1,19 @@
 import ads1115
 
+
 class VernierODO(ads1115.ADS1115):
-    
     def __init__(self, *args, **kwargs):
         super(VernierODO, self).__init__(*args, **kwargs)
-        
-        
+
     def convertMGL(self, adc):
-        c = adc * (5/26665.8528646) #Converts to a 0-5V range
-        output = (c * 4.444) - .4444 #Converts to mg/L based of Vernier's scale
-        
+        c = adc * (5/26665.8528646)  # Converts to a 0-5V range
+        # Converts to mg/L based of Vernier's scale
+        output = (c * 4.444) - .4444
+
         return output
-        
-    
+
     def read(self):
         adc = super(VernierODO, self).read()
         oxygenLevel = self.convertMGL(adc)
-        
-        return {'rawADC' : adc, 'mgL': oxygenLevel}
-    
+
+        return {'rawADC': adc, 'mgL': oxygenLevel}
