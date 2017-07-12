0d291255247c30f3a81ee1c05df0a4c86bdb4ae0
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-07-11 21:09:32 -0400

    Move methods in control server to a class

--- rover/control_server/control_server.py
+++ rover/control_server/control_server.py
@@ -1,6 +1,8 @@
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, send_from_directory
 from pymongo import MongoClient
+from aesrdevicelib.sensors.blue_esc import BlueESC
+from aesrdevicelib.sensors.bno055 import BNO055
 import time
 import os
 import sys
@@ -17,152 +19,154 @@ CONTROL_MODE = "R"
 DEBUG = True
 WEBSERVER_FOLDER_NAME = "motorwebserver"
 
-# Dynamic Variables
-app = Flask(__name__, static_folder=WEBSERVER_FOLDER_NAME + "/static",
-            template_folder=WEBSERVER_FOLDER_NAME + "/templates")
-socketio = SocketIO(app)
 
-# BlueESC instances
-try:
-    from aesrdevicelib.sensors.blue_esc import BlueESC
-    motors = {"f": BlueESC(0x2a), "b": BlueESC(0x2d), "l": BlueESC(0x2b),
-              "r": BlueESC(0x2c)}
-except:
-    print("Motor setup error: " + str(sys.exc_info()[1]))
-    print("Disabling motors...")
-    motors = None
-
-# BNO055 sensor setup
-try:
-    from aesrdevicelib.sensors.bno055 import BNO055
-    imu = BNO055()
-    time.sleep(1)
-    imu.setExternalCrystalUse(True)
-except:
-    print("\nIMU setup error: " + str(sys.exc_info()[1]))
-    print("Disabling IMU...")
-    imu = None
-
-lastConnectTime = None
-
-previousStatusData = []
-
-# Get status Collection object from inputted database name
-try:
-    dbCol = (MongoClient()[sys.argv[1]])["status"]
-except:
-    dbCol = None
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
-
-
-@app.route('/')
-def index():
-    """Serve the client-side application."""
-    return render_template('index.html')
-
-
-def getStatusData():
-    statusData = []
-    for data in dbCol.find().sort([["_id", 1]]):
-        del data[u'_id']
-        statusData.append(data)
-    return statusData
-
-
-def normalizeMotorPower(power):
+def normalize_motor_power(power):
     max_ = 10
     min_ = -max_
     return int(max(min_, min(max_, power)))
 
 
-@socketio.on('connect')
-def connect():
-    print("connect")
+class ControlServer(SocketIO):
+    def __init__(self, mongo_db=None):
+        # Dynamic Variables
+        self.app = Flask(__name__, static_folder=WEBSERVER_FOLDER_NAME + "/static",
+                         template_folder=WEBSERVER_FOLDER_NAME + "/templates")
 
+        super().__init__(self.app)
 
-# NOTE: When getting data from VirtualJoystick, make sure to check if it is
-# "up", "down", "left", or "right" to stop when finger is lifted off
-@socketio.on('input')
-def inputControl(data):
-    targetBearing = 10
-    gain = 10  #32767/80
-    xValue = int(data['x']*gain)
-    yValue = int(data['y']*gain)
+        # BlueESC instances
+        try:
+            self.motors = {"f": BlueESC(0x2a), "b": BlueESC(0x2d), "l": BlueESC(0x2b),
+                           "r": BlueESC(0x2c)}
+        except IOError:
+            print("Motor setup error: " + str(sys.exc_info()[1]))
+            print("Disabling motors...")
+            self.motors = None
 
-    print("\n====================================")
-    print("Joy X:", xValue, "|", "Joy Y:", yValue)
+        # BNO055 sensor setup
+        try:
+            self.imu = BNO055()
+            time.sleep(1)
+            self.imu.setExternalCrystalUse(True)
+        except IOError:
+            print("\nIMU setup error: " + str(sys.exc_info()[1]))
+            print("Disabling IMU...")
+            self.imu = None
 
-    # IMU Compass:
-    compass = None
-    currentBearing = None
-    compassTorque = 0
-    if imu is not None:
-        compass = imu.getVector(BNO055.VECTOR_MAGNETOMETER)
-        currentBearing = math.atan2(compass[1], compass[0]) * 180 / math.pi
-        compassTorque = (((currentBearing - targetBearing) + 180) % 360) - 180
+        self.lastConnectTime = None
+        self.previousStatusData = []
 
-    print("Bearing: " + str(currentBearing))
-    print("Compass Torque: " + str(compassTorque))
+        # Get status Collection object from inputted database name
+        try:
+            self.dbCol = (MongoClient()[mongo_db])["status"]
+        except:
+            self.dbCol = None
 
-    # Motor power calculation:
-    torque = 0
-    if CONTROL_MODE == "R":
-        torque = xValue
-        motorPower = {'f': torque, 'b': -torque,
-                      'l': yValue, 'r': yValue}
+        # Routes:
+        self.favicon = self.app.route('/favicon.ico')(self.favicon)
+        self.index = self.app.route('/')(self.index)
 
-    else:  # Strafe mode
-        torque = compassTorque
-        motorPower = {'f': xValue + torque, 'b': xValue - torque,
-                      'l': yValue + torque, 'r': yValue - torque}
+        # Socket endpoints:
+        self.connect = self.on('connect')(self.connect)
+        self.inputControl = self.on('input')(self.inputControl)
+        self.poll = self.on('poll')(self.poll)
+        self.client_disconnect = self.on('disconnect')(self.client_disconnect)
 
-    print("Torque: " + str(torque))
+    def run(self, host=None, port=None, **kwargs):
+        super().run(self.app, host, port, **kwargs)
 
-    motorPower = {k: normalizeMotorPower(v) for k, v in motorPower.items()}
+    def favicon(self):
+        return send_from_directory(os.path.join(self.app.root_path, 'static'),
+                                   'favicon.ico',
+                                   mimetype='image/vnd.microsoft.icon')
 
-    # Print motor speeds:
-    print("\nMotors: ")
-    print("F: " + str(motorPower['f']))
-    print("B: " + str(motorPower['b']))
-    print("L: " + str(motorPower['l']))
-    print("R: " + str(motorPower['r']))
-    print("====================================")
+    @staticmethod
+    def index():
+        """Serve the client-side application."""
+        return render_template('index.html')
 
-    # Update motor speeds if they were setup correctly:
-    if isinstance(motors, dict):
-        # X plane motors
-        motors['f'].startPower(motorPower['f'])
-        motors['b'].startPower(motorPower['b'])
+    def get_status_data(self):
+        status_data = []
+        for data in self.dbCol.find().sort([["_id", 1]]):
+            del data[u'_id']
+            status_data.append(data)
+        return status_data
 
-        # Y plane motors
-        motors['l'].startPower(motorPower['l'])
-        motors['r'].startPower(motorPower['r'])
+    @staticmethod
+    def client_connect():
+        print("connect")
 
-    # Emit status data if collection was supplied:
-    if dbCol is not None:
-        emit("status", getStatusData())
+    # NOTE: When getting data from VirtualJoystick, make sure to check if it is
+    # "up", "down", "left", or "right" to stop when finger is lifted off
+    def input_control(self, data):
+        target_bearing = 10
+        gain = 10  # 32767/80
+        x_value = int(data['x']*gain)
+        y_value = int(data['y']*gain)
 
+        print("\n====================================")
+        print("Joy X:", x_value, "|", "Joy Y:", y_value)
 
-@socketio.on('poll')
-def poll(data):
-    global lastConnectTime
-    lastConnectTime = time.time()
+        # IMU Compass:
+        current_bearing = None
+        compass_torque = 0
+        if self.imu is not None:
+            compass = self.imu.getVector(BNO055.VECTOR_MAGNETOMETER)
+            current_bearing = math.atan2(compass[1], compass[0]) * 180 / math.pi
+            compass_torque = (((current_bearing - target_bearing) + 180) % 360) - 180
 
+        print("Bearing: " + str(current_bearing))
+        print("Compass Torque: " + str(compass_torque))
 
-@socketio.on('disconnect')
-def disconnect():
-    print('disconnect')
+        # Motor power calculation:
+        if CONTROL_MODE == "R":
+            torque = x_value
+            motor_power = {'f': torque, 'b': -torque,
+                           'l': y_value, 'r': y_value}
+
+        else:  # Strafe mode
+            torque = compass_torque
+            motor_power = {'f': x_value + torque, 'b': x_value - torque,
+                           'l': y_value + torque, 'r': y_value - torque}
+
+        print("Torque: " + str(torque))
+
+        motor_power = {key: normalize_motor_power(val) for key, val in motor_power.items()}
+
+        # Print motor speeds:
+        print("\nMotors: ")
+        print("F: " + str(motor_power['f']))
+        print("B: " + str(motor_power['b']))
+        print("L: " + str(motor_power['l']))
+        print("R: " + str(motor_power['r']))
+        print("====================================")
+
+        # Update motor speeds if they were setup correctly:
+        if isinstance(self.motors, dict):
+            # X plane motors
+            self.motors['f'].startPower(motor_power['f'])
+            self.motors['b'].startPower(motor_power['b'])
+
+            # Y plane motors
+            self.motors['l'].startPower(motor_power['l'])
+            self.motors['r'].startPower(motor_power['r'])
+
+        # Emit status data if collection was supplied:
+        if self.dbCol is not None:
+            emit("status", self.get_status_data())
+
+    def poll(self, data):
+        self.lastConnectTime = time.time()
+
+    @staticmethod
+    def client_disconnect():
+        print('disconnect')
 
 if __name__ == '__main__':
+    cs = ControlServer()
     try:
-        socketio.run(app, host="0.0.0.0", port=8000)
+        cs.run(host="0.0.0.0", port=8000)
     except KeyboardInterrupt:
-        if isinstance(motors, dict):
-            for k, v in motors:
-                motors[k].setPower(0)
+        if isinstance(cs.motors, dict):
+            for k, v in cs.motors:
+                cs.motors[k].setPower(0)
